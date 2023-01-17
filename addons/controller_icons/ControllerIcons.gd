2383b962d9477848e829df65ead73979c596798a
Ricardo Subtil <ricasubtil@gmail.com>
2023-01-17 15:05:45 +0000

    Propagate icon changes on first frame, and throuhg manual method

--- addons/controller_icons/ControllerIcons.gd
+++ addons/controller_icons/ControllerIcons.gd
@@ -11,7 +11,7 @@ enum InputType {
 var _cached_icons := {}
 var _custom_input_actions := {}
 
-var _last_input_type = InputType.KEYBOARD_MOUSE
+var _last_input_type
 var _settings
 var _file := File.new()
 
@@ -47,6 +47,14 @@ func _ready():
 	if _settings.custom_mapper:
 		Mapper = _settings.custom_mapper.new()
 
+	# Wait a frame to give a chance for the app to initialize
+	yield(get_tree(), "idle_frame")
+	# Set input type to what's likely being used currently
+	if Input.get_connected_joypads().empty():
+		_set_last_input_type(InputType.KEYBOARD_MOUSE)
+	else:
+		_set_last_input_type(InputType.CONTROLLER)
+
 func _on_joy_connection_changed(device, connected):
 	if device == 0:
 		if connected:
@@ -77,6 +85,10 @@ func _input(event: InputEvent):
 
 func _add_custom_input_action(input_action: String, data: Dictionary):
 	_custom_input_actions[input_action] = data["events"]
+
+func refresh():
+	# All it takes is to signal icons to refresh paths
+	emit_signal("input_type_changed", _last_input_type)
 
 func parse_path(path: String) -> Texture:
 	var root_paths := _expand_path(path)
